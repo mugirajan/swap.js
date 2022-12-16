e9f599c6d8e4a26d406fea13ab15c1d27b720983
mugilavathi <mugirajan95@gmail.com>
2022-12-16 09:34:29 +0530

    add my swap

---
+++ main.js
@@ -0,0 +1,19 @@
+/*let firstvalue = "mugi";
+let secondvalue= "rajan";
+let swap;
+
+//swap variables
+swap = firstvalue;
+firstvalue = secondvalue;
+secondvalue= swap;
+
+document.write("The swap value :"   + (firstvalue)+"<br>");
+document.write("The swap value :"   + (secondvalue));
+*/
+function swap(name,sec){
+        proof=name;
+        name=sec;
+        sec=proof;
+        console.log(name,sec)
+}
+    swap("mugi","fed04")
